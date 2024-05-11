cf23bf59fa79624f5d40f31058b35030141858ac
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-05-11 23:39:36 +0300

    add mobile logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@
 // }
 
 import { Logo } from './components/Logo'
+// import { WelcomeSection } from './components/WelcomeSection/WelcomeSection'
 
 export const App = () => {
   return (
     <div>
-      {' '}
-      <Logo />{' '}
+      <Logo />
+      {/* <WelcomeSection /> */}
     </div>
   )
 }
---
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -0,0 +1,14 @@
+// import { Logo } from '../Logo'
+// import css from './WelcomeSection.module.css'
+
+// export const WelcomeSection = () => {
+//   return (
+//     <div className={css.container}>
+//       <Logo />
+//       <p>Record daily water intake and track</p>
+//       <h2>Water consumption tracker</h2>
+//       <button className={css.trytracker}>Try tracker</button>
+//       <button className={css.signin}>Sign In</button>
+//     </div>
+//   )
+// }
