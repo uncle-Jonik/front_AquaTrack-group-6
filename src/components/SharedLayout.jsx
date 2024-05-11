d0c328d267fc5595c52871dc63bc053d0fe67a8c
Shchyrba Olga <olga.binder@gmail.com>
2024-05-12 00:53:57 +0300

    WaterMainInfa_section

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -3,7 +3,6 @@ import css from "./SharedLayout.module.css";
 const SharedLayout = ({ children }) => {
   return (
     <div className={css.mainWrapper}>
-      <h1>Logo</h1>
       <div className={css.LayoutWrupper}>{children}</div>
     </div>
   );
--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,7 +1,20 @@
+import css from "./WaterMainInfo.module.css";
+import { TiPlus } from "react-icons/ti";
+
 const WaterMainInfo = () => {
   return (
-    <div>
-      <h2>WaterMainInfo</h2>
+    <div className={css.WaterMainInfoWrapper}>
+      <div className={css.wrapper}>
+        <h2 className={css.title}>1.5 L</h2>
+        <p className={css.text}>My daily norma</p>
+      </div>
+      <div className={css.img}>
+        <img src="/src/assets/img/bottle@1x.png" alt="Бутилка води" />
+      </div>
+      <button>
+        <TiPlus />
+        Add water
+      </button>
     </div>
   );
 };
