833691bf77896c8c6117b25c77d61e75f8197e41
Shchyrba Olga <olga.binder@gmail.com>
2024-05-12 09:05:42 +0300

    WaterBar

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,7 +1,14 @@
 import css from "./WaterMainInfo.module.css";
 import { TiPlus } from "react-icons/ti";
+import { useState } from "react";
 
 const WaterMainInfo = () => {
+  const [waterLevel, setWaterLevel] = useState(0);
+
+  const handleAddWater = () => {
+    setWaterLevel((prevLevel) => prevLevel + 10);
+  };
+
   return (
     <div className={css.WaterMainInfoWrapper}>
       <div className={css.wrapper}>
@@ -9,9 +16,27 @@ const WaterMainInfo = () => {
         <p className={css.text}>My daily norma</p>
       </div>
       <div className={css.img}>
-        <img src="/src/assets/img/bottle@1x.png" alt="Бутилка води" />
+        <img
+          srcSet="/src/assets/img/bottle@1x.png 1x, /src/assets/img/bottle@2x.png 2x"
+          src="photo.jpg"
+          alt="Бутилка води"
+        />
       </div>
-      <button>
+
+      <div className={css.waterProgressBar}>
+        <h2 className={css.barTitle}>Today</h2>
+        <input
+          type="range"
+          className={css.rangeInput}
+          min="0"
+          max="100"
+          value={waterLevel}
+          onChange={(e) => setWaterLevel(e.target.value)}
+        />
+        <span className={css.progressLabel}>{waterLevel}%</span>
+      </div>
+
+      <button onClick={handleAddWater}>
         <TiPlus />
         Add water
       </button>
