a14e063282a9b60857448d593a04868a889e3b3d
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-15 22:27:27 +0300

    fix
    Co-Authored-By: ElizavetaRevina139 <139202899+Elizaveta139@users.noreply.github.com>
    Co-Authored-By: Ruslan <120032162+INRGI@users.noreply.github.com>
    Co-Authored-By: Olha-PSM <122490056+Olha-PSM@users.noreply.github.com>
    Co-Authored-By: Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
    Co-Authored-By: vl-brrr <138128625+vl-brrr@users.noreply.github.com>
    Co-Authored-By: Yelyzaveta Demchenko <138761874+yd1224@users.noreply.github.com>

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -5,7 +5,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
-import { registerUsers } from "../../redux/auth/operations";
+import { registerUser } from "../../redux/user/userOperations";
 
 const schema = yup.object().shape({
   email: yup
@@ -48,7 +48,7 @@ export function SignUpForm() {
           className={css.form}
           onSubmit={handleSubmit((data) => {
             console.log(data);
-            dispatch(registerUsers(data));
+            dispatch(registerUser(data));
             reset();
           })}
         >
