686898cdcb0d6272792bda4d96844dae78824905
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-15 15:25:12 +0300

    proses

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
+import { fetchRegister } from "./api/usersAPI";
 
 export const App = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function testing() {}
+    const controller = new AbortController();
+    const a = { email: "bob@i.ua", password: "Bob_2024" };
+
+    async function testing() {
+      const res = await fetchRegister(a, { abortController: controller });
+
+      setData(res.data);
+    }
 
     testing();
   }, []);
+
+  console.log(data);
   return (
     <>
       <b>s</b>
--- src/api/usersAPI.js
+++ src/api/usersAPI.js
@@ -1,1 +1,10 @@
 import axios from "axios";
+
+axios.defaults.baseURL =
+  "https://back-aquatrack-group-6.onrender.com/api/users";
+
+export async function fetchRegister(data, { abortController }) {
+  const res = axios({ method: "post" });
+
+  return res;
+}
