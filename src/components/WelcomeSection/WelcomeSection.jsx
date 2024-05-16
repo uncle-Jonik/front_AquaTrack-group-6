7676c9f57e5b9e95d1155d2813a7f4d44efd3f94
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-16 14:29:01 +0300

    logo fix

--- src/components/WelcomeSection/WelcomeSection.jsx
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -11,12 +11,13 @@ export const WelcomeSection = () => {
         <p className={css.welcomePhrase}>Record daily water intake and track</p>
         <h1 className={css.welcomeName}>Water consumption tracker</h1>
         <div className={css.welcomeButton_blok}>
-          <button className={css.welcomeButton_tracker}>
-            <Link to="/signup">Try tracker</Link>
-          </button>
-          <button className={css.welcomeButton_sign}>
-            <Link to="/signin">Sign In</Link>
-          </button>
+          <Link to="/signup" className={css.welcomeButton_tracker}>
+            Try tracker
+          </Link>
+
+          <Link to="/signin" className={css.welcomeButton_sign}>
+            Sign In
+          </Link>
         </div>
       </div>
     </div>
