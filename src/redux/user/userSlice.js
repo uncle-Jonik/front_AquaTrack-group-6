8e7eafa85653f11aaeeae2dba194149497a18cfa
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-18 23:56:57 +0300

    loader + redux
    Co-Authored-By: vl-brrr <138128625+vl-brrr@users.noreply.github.com>
    Co-Authored-By: Ruslan <120032162+INRGI@users.noreply.github.com>

--- src/redux/user/userSlice.js
+++ src/redux/user/userSlice.js
@@ -36,16 +36,18 @@ const userSlice = createSlice({
       })
       .addCase(loginUser.fulfilled, (state, action) => {
         state.isLoggedIn = true;
+        state.isRefreshing = false;
         state.accessToken = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
 
-        console.log(action.payload);
         state.userInfo = action.payload.user;
       })
+
       .addCase(logoutUser.fulfilled, (state) => {
         state.isLoggedIn = false;
         //////////////////////////////
       })
+
       .addCase(refreshUser.pending, (state, action) => {
         //////////////////////////////
         // ТУТ МАЄ ВАШ ЛОУДЕР;
@@ -57,8 +59,17 @@ const userSlice = createSlice({
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+
         state.accessToken = action.payload.accessToken;
         state.refreshToken = action.payload.refreshToken;
+
+        state.userInfo.email = action.payload.user.email;
+        state.userInfo.name = action.payload.user.name;
+        state.userInfo.gender = action.payload.user.gender;
+        state.userInfo.avatar = action.payload.user.avatarUrl;
+        state.userInfo.weight = action.payload.user.weight;
+        state.userInfo.sportsActivity = action.payload.user.sportsActivity;
+        state.userInfo.waterRate = action.payload.user.waterRate;
       })
 
       .addCase(updateUser.pending, (state, action) => {
@@ -79,6 +90,7 @@ const userSlice = createSlice({
         state.userInfo.sportsActivity = action.payload.sportsActivity;
         state.userInfo.waterRate = action.payload.waterRate;
       })
+
       .addCase(updateUser.rejected, (state, action) => {
         state.error = action.payload;
       })
