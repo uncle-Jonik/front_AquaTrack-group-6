0556ccb9c89e981b78ed250024ba0ec8b3088d3a
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-19 15:28:16 +0300

    add comp + css

---
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -0,0 +1,33 @@
+import css from "./WaterMainInfo.module.css";
+
+import { Logo } from "../Logo";
+import { AddWaterBtn } from "../AddWaterBtn/AddWaterBtn";
+
+export const WaterMainInfo = () => {
+  return (
+    <div className={css.wrapper}>
+      <Logo />
+
+      <div className={css.statusBar}>
+        <p>Today</p>
+
+        <input
+          type="range"
+          className={css.rangeInput}
+          min="0"
+          max="100"
+          value={50}
+          // onChange={(e) => setWaterLevel(e.target.value)}
+        />
+
+        <div className={css.labels}>
+          <span>0%</span>
+          <span>50%</span>
+          <span>100%</span>
+        </div>
+      </div>
+
+      <AddWaterBtn section="daily" />
+    </div>
+  );
+};
--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -3,35 +3,18 @@ import css from "./TrackerPage.module.css";
 import { WaterDetailedInfo } from "../../components/WaterDetailedInfo/WaterDetailedInfo.jsx";
 import { useAuth } from "../../hooks/useAuth.jsx";
 import { useWater } from "../../hooks/useWater.jsx";
-// import { WaterMainInfo } from "../../components/WaterMainInfo/WaterMainInfo.jsx";
-// import { useDispatch } from "react-redux";
-// import { useEffect } from "react";
-// import { useWater } from "../../hooks/useWater.jsx";
-// import { fetchWaterPerDay } from "../../redux/water/waterOperations.js";
 import { Loader } from "../../components/Loader/Loader.jsx";
+import { WaterMainInfo } from "../../components/WaterMainInfo/WaterMainInfo.jsx";
 
 export default function TrackerPage() {
-  // const dispatch = useDispatch();
-  // const { isRefreshing } = useSelector(selectIsRefreshing);
-
   const refreshUser = useAuth().isRefreshing;
   const refreshWater = useWater().isLoading;
-  //   const water = useWater();
-
-  //   useEffect(() => {
-  //     const date = new Date();
-  //     dispatch(
-  //       fetchWaterPerDay(
-  //         `${date.getDate()}.0${date.getMonth()}.${date.getFullYear()}`
-  //       )
-  //     );
-  //   }, [dispatch]);
 
   return (
     <div className={css.wrapper}>
       {(refreshUser || refreshWater) && <Loader absolute={true} />}
 
-      {/* <WaterMainInfo /> */}
+      <WaterMainInfo />
       <WaterDetailedInfo />
     </div>
   );
