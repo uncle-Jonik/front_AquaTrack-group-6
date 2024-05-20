27a1a2d52e8d60df38b257f1ab17d480b0dd1584
Serhii2009 <137280424+Serhii2009@users.noreply.github.com>
2024-05-20 12:52:12 +0300

    fix second section

--- src/components/AdvantagesSection/AdvantagesSection.jsx
+++ src/components/AdvantagesSection/AdvantagesSection.jsx
@@ -2,43 +2,52 @@ import css from './AdvantagesSection.module.css'
 
 export const AdvantagesSection = () => {
   return (
-    <div className={css.advantagesSection}>
-      <img
-        className={css.advantagesPhoto}
-        src="../../../src/assets/img/baner@1x.jpg"
-        srcSet="../../../src/assets/img/baner@1x.jpg 1x, ../../baner@2x.jpg"
-        alt="Woman drink water"
-        loading="lazy"
-      />
+    <div className={css.advantages_main}>
+      <div className={css.advantages_users}>
+        <ul className={css.advantages_usersImg}>
+          <li className={css.advantages_point}>
+            <img
+              className={css.user}
+              src="../../../src/assets/img/user1.png"
+              alt="user1"
+            />
+          </li>
 
-      <div className={css.advantagesUsers}>
-        <div className={css.advantagesBlock}>
-          <img
-            src="../../../src/assets/img/user@1x.png"
-            srcSet="../../../src/assets/img/user@1x.png 1x, ../../user@2x.png 2x"
-            alt="Users"
-            loading="lazy"
-          />
-          <p className={css.advantagesText}>
-            Our <span className={css.advantagesSpan}>happy</span> customers
-          </p>
-        </div>
+          <li className={css.advantages_point}>
+            <img
+              className={css.user}
+              src="../../../src/assets/img/user2.png"
+              alt="user2"
+            />
+          </li>
+
+          <li className={css.advantages_point}>
+            <img
+              className={css.user}
+              src="../../../src/assets/img/user3.png"
+              alt="user3"
+            />
+          </li>
+        </ul>
+
+        <p className={css.advantagesText}>
+          Our <span className={css.textSpan}>happy</span> customers
+        </p>
       </div>
 
-      <div className={css.advantagesHabits}>
-        <div className={css.advantagesHabits_inoneline}>
-          <div className={css.advantagesHabits_1}>
-            <div className={css.advantagesHabits_1_sircle}></div>
-            <p className={css.advantagesHabits_1_text}>Habit drive</p>
-          </div>
-          <div className={css.advantagesHabits_2}>
-            <p className={css.advantagesHabits_2_text}>View statistics</p>
-          </div>
-        </div>
-
-        <div className={css.advantagesHabits_3}>
-          <p className={css.advantagesHabits_3_text}>Personal rate setting</p>
-        </div>
+      <div className={css.advantagesGroup_habits}>
+        <ul className={css.advantagesTabs}>
+          <li className={css.advantages_habits}>
+            <div className={css.ellipse}></div>
+            <p className={css.habit_1}>Habit drive</p>
+          </li>
+          <li className={css.advantages_habits}>
+            <p className={css.habit_2_3}>View statistics</p>
+          </li>
+          <li className={css.advantages_habits}>
+            <p className={css.habit_2_3}>Personal rate setting</p>
+          </li>
+        </ul>
       </div>
     </div>
   )
