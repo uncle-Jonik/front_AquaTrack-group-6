2d0b38f1f5c1ae5a080f96fe09670c6e46afd9ad
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-22 01:21:49 +0300

    sign in
    Co-Authored-By: Ruslan <120032162+INRGI@users.noreply.github.com>
    Co-Authored-By: Shchyrba Olga <121376394+Olguca2020@users.noreply.github.com>
    Co-Authored-By: Olha-PSM <122490056+Olha-PSM@users.noreply.github.com>
    Co-Authored-By: ElizavetaRevina139 <139202899+Elizaveta139@users.noreply.github.com>
    Co-Authored-By: vl-brrr <138128625+vl-brrr@users.noreply.github.com>

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,17 +1,15 @@
 // import { Logo } from "../../components/Logo";
-import css from './SignInPage.module.css'
-import { AdvantagesSection } from '../../components/AdvantagesSection/AdvantagesSection'
-import { SignInForm } from '../../components/SignInForm/SignInForm'
+import css from "./SignInPage.module.css";
+import { AdvantagesSection } from "../../components/AdvantagesSection/AdvantagesSection";
+import { SignInForm } from "../../components/SignInForm/SignInForm";
 
 export default function SignInPage() {
   return (
     <div className={css.generalSignInForm}>
-      {/* <Logo /> */}
       <SignInForm />
-
       <div className={css.AdvantagesSection_in}>
         <AdvantagesSection />
       </div>
     </div>
-  )
+  );
 }
