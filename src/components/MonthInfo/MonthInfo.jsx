98b5319ed8d256efa720e8e1d3e8f915b2da6764
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-22 22:16:51 +0300

    a

--- src/components/MonthInfo/MonthInfo.jsx
+++ src/components/MonthInfo/MonthInfo.jsx
@@ -1,19 +1,24 @@
 import Calendar from "../Calendar";
 import CalendarPagination from "../CalendarPagination";
-
-import css from './MonthInfo.module.css'
+import css from "./MonthInfo.module.css";
+import { FaRegCalendarAlt } from "react-icons/fa";
 
 const MonthInfo = () => {
-    return (
-        <section className={css.monthSectionInfo}>
-            <div className={css.monthPaginationContainer}>
-                <h3 className={css.monthTitle}>Month</h3>
-                <CalendarPagination />
-            </div>
-            
-            <Calendar />
-        </section>
-        )
-}
+  return (
+    <section className={css.monthSectionInfo}>
+      <div className={css.monthPaginationContainer}>
+        <h3 className={css.monthTitle}>Month</h3>
+        <div className={css.pag_btn}>
+          <CalendarPagination />
+          <button>
+            <FaRegCalendarAlt />
+          </button>
+        </div>
+      </div>
+
+      <Calendar />
+    </section>
+  );
+};
 
 export default MonthInfo;
