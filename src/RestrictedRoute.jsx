7a68574fdc167443d9bb2cbe5b15991799eebe49
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-05-23 18:57:19 +0200

    some fix

--- src/RestrictedRoute.jsx
+++ src/RestrictedRoute.jsx
@@ -2,6 +2,11 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "./hooks/useAuth.jsx";
 
 export const RestrictedRoute = ({ component: Component, redirectTo = "/" }) => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn, isRefreshing } = useAuth();
+
+  if (isRefreshing) {
+    return null;
+  }
+
   return isLoggedIn ? <Navigate to={redirectTo} /> : Component;
 };
