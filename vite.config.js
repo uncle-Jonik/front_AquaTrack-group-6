c3f56805ebeaf31a7bf08aa5edeaae0658199341
Yevgenii <126769681+uncle-Jonik@users.noreply.github.com>
2024-05-24 12:14:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,27 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgr from "vite-plugin-svgr";
+
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: true,
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      src: "/src",
+      components: "/src/components",
+      pages: "/src/pages",
+      assets: "/src/assets",
+    },
   },
+  base: "/front_AquaTrack-group-6/",
 });
